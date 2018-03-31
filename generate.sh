01eb193cc1e0d532c5ff604de0357ffd19402d72
Tjeerd Jan van der Molen <tjeerd.jan.van.der.molen@ordina.nl>
2018-03-31 23:25:32 +0200

    updated to docker multi stage

--- generate.sh
+++
@@ -1,6 +0,0 @@
-CERTI_VERSION=3.5.1
-INSTALL_DIR=$PWD/bin
-docker build -f Build --build-arg CERTI_VERSION=$CERTI_VERSION -t aidamina/certi-builder:latest . 
-docker run -it -v $INSTALL_DIR:/install aidamina/certi-builder:latest
-docker build -t aidamina/certi:$CERTI_VERSION .
-
